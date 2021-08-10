5c7428d992cb56183b15f6a17e015deaddf6b47c
MuhammadFadll <65841599+fadelun@users.noreply.github.com>
2021-08-10 10:36:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let result = document.querySelector('.output');
+
+let expression = '';
+
+
+const press = (e) => {
+            expression += e
+            result.innerHTML = expression;
+}
+
+const equal = (e) => {
+    result.innerHTML = eval(expression);
+    expression = ''
+}
+
+const remove = (e) => {
+    expression = ''
+    result.innerHTML = expression;
+}
+
+
+
+
+
